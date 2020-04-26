a7649959c1508b962944ce4800f9f4eaa1160a96
aoisupersix <aoisupersix@gmail.com>
2020-04-26 19:22:57 +0900

    initial commit

---
+++ src/content_script.ts
@@ -0,0 +1,25 @@
+import $ from 'jquery'
+
+/**
+ * save-formで表示されている残労働時間を取得します。
+ * 取得できない場合は空文字を返します
+ */
+const getRemainingWorkingTime = (): string => {
+    const $unMatchTime = $('div#un-match-time')
+    if (!$unMatchTime) { return ''}
+    const unMatchTime = $unMatchTime.text().match(/\d{1,}:\d{1,}/)
+    if (unMatchTime !== null) {
+        return unMatchTime[0]
+    }
+
+    return ''
+}
+
+$(document).on('click', 'div.btn.btn-default', (event) => {
+    if (event.currentTarget.textContent !== '＋') { return }
+    $('input.man-hour-input').each((_, element) => {
+        if($(element).val() !== '') { return }
+        $(element).val(getRemainingWorkingTime())
+    })
+})
+
