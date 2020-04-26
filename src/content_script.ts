3c44ceefbfdcc08a1fde82c12f091b1899614c28
aoisupersix <aoisupersix@gmail.com>
2020-04-26 19:49:27 +0900

    [add] code formatter

--- src/content_script.ts
+++ src/content_script.ts
@@ -5,21 +5,26 @@ import $ from 'jquery'
  * 取得できない場合は空文字を返します
  */
 const getRemainingWorkingTime = (): string => {
-    const $unMatchTime = $('div#un-match-time')
-    if (!$unMatchTime) { return ''}
-    const unMatchTime = $unMatchTime.text().match(/\d{1,}:\d{1,}/)
-    if (unMatchTime !== null) {
-        return unMatchTime[0]
-    }
-
+  const $unMatchTime = $('div#un-match-time')
+  if (!$unMatchTime) {
     return ''
+  }
+  const unMatchTime = $unMatchTime.text().match(/\d{1,}:\d{1,}/)
+  if (unMatchTime !== null) {
+    return unMatchTime[0]
+  }
+
+  return ''
 }
 
 $(document).on('click', 'div.btn.btn-default', (event) => {
-    if (event.currentTarget.textContent !== '＋') { return }
-    $('input.man-hour-input').each((_, element) => {
-        if($(element).val() !== '') { return }
-        $(element).val(getRemainingWorkingTime())
-    })
+  if (event.currentTarget.textContent !== '＋') {
+    return
+  }
+  $('input.man-hour-input').each((_, element) => {
+    if ($(element).val() !== '') {
+      return
+    }
+    $(element).val(getRemainingWorkingTime())
+  })
 })
-
