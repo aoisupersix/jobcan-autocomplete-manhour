87256f7aedde5c7a1d7560ef4d5a74c88f81309a
aoisupersix <aoisupersix@gmail.com>
2020-05-06 21:19:33 +0900

    [clean] format webpack config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,41 +1,39 @@
-const webpack = require("webpack");
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const srcDir = '../src/';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const webpack = require('webpack')
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+const srcDir = '../src/'
 
 module.exports = {
-    entry: {
-        content_script: path.join(__dirname, srcDir + 'content_script.ts')
+  entry: {
+    /* eslint-disable-next-line @typescript-eslint/camelcase */
+    content_script: path.join(__dirname, srcDir + 'content_script.ts'),
+  },
+  output: {
+    path: path.join(__dirname, '../dist/js'),
+    filename: '[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial',
     },
-    output: {
-        path: path.join(__dirname, '../dist/js'),
-        filename: '[name].js'
-    },
-    optimization: {
-        splitChunks: {
-            name: 'vendor',
-            chunks: "initial"
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    plugins: [
-        // exclude locale files in moment
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new CopyPlugin([
-            { from: '.', to: '../' }
-          ],
-          {context: 'public' }
-        ),
-    ]
-};
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    // exclude locale files in moment
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new CopyPlugin([{ from: '.', to: '../' }], { context: 'public' }),
+  ],
+}
